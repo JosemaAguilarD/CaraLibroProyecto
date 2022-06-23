4ec111c53314087e7782284dc416573cda8ea11b
josemariaaguilar0899@gmail.com <josemariaaguilar0899@gmail.com>
2022-06-23 00:55:17 -0400

    Primer Commit

---
+++ CaraLibro/LoginViewController.swift
@@ -0,0 +1,11 @@
+//
+//  LoginViewController.swift
+//  CaraLibro
+//
+//  Created by user191299 on 6/23/22.
+//
+
+import UIKit
+class LoginViewController: UIViewController{
+    
+}
---
+++ CaraLibro/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CaraLibro
+//
+//  Created by user191299 on 6/22/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
