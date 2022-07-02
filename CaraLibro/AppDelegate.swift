8768275de65a777e6c766919874e41be2eb6d396
Josemaria <josemariaaguilar0899@gmail.com>
2022-07-02 05:06:16 -0400

    Configuracion Firebase

--- CaraLibro/AppDelegate.swift
+++ CaraLibro/AppDelegate.swift
@@ -4,19 +4,23 @@
 //
 //  Created by user191299 on 6/22/22.
 //
-
+import SwiftUI
 import UIKit
+import FirebaseCore
 
-@main
+@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+      
+        FirebaseApp.configure()
+        print("hola")
+
         return true
     }
 
+    
+    
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
@@ -30,7 +34,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
+    
+ 
 }
-
