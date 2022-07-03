29d1385bada88ce0df81bdbc980ad78d75c7d1b8
Josemaria <josemariaaguilar0899@gmail.com>
2022-07-03 05:22:27 -0400

    Integracion Firebase y Logueo

--- CaraLibro/LoginViewController.swift
+++ CaraLibro/LoginViewController.swift
@@ -3,15 +3,44 @@
 //  CaraLibro
 //
 //  Created by user19129
-import SwiftUI
 import UIKit
+import FirebaseAnalytics
 import FirebaseAuth
+enum ProviderType: String{
+    case basic
+}
 class LoginViewController: UIViewController{
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var contrasenaTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
     @IBAction private func tap_Action(sender:UITapGestureRecognizer) {
             self.view.endEditing(true)
     }
+    
+
+    @IBAction func LoginButtonAction(_ sender: Any) {
+        if let email = emailTextField.text, let contrasena = contrasenaTextField.text{
+            Auth.auth().signIn(withEmail: email, password: contrasena) { (result, error) in
+                
+                if let result = result, error == nil{
+                
+                    let alertController = UIAlertController(title: "Logueado", message: "Se ha logueado correctamente", preferredStyle: .alert)
+                    alertController.addAction(UIAlertAction(title: "Aceptar", style: .default))
+                    self.present(alertController, animated: true, completion: nil)
+                }else{
+                    let alertController = UIAlertController(title: "Error", message: "Se ha producido un error registrando al usuario", preferredStyle: .alert)
+                    alertController.addAction(UIAlertAction(title: "Aceptar", style: .default))
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+             super.viewDidLoad()
+         }
+  
     
   /*  enum ProviderType: String {
            case basic
@@ -21,9 +50,7 @@ class LoginViewController: UIViewController{
        @IBOutlet weak var apellidoText: UITextField!
        @IBOutlet weak var botonLogueo: UIButton!
  
-    override func viewDidLoad() {
-             super.viewDidLoad()
-         }
+   
    */
          
       //   @IBAction func logearActionButon(_ sender: Any) {
