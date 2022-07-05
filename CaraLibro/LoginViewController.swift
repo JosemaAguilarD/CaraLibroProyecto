7589e3cd07c3a8a3e39523bb0927f918266d7999
Josemaria <josemariaaguilar0899@gmail.com>
2022-07-05 09:42:09 -0400

    Implemetación de Recuperación de Contraseña

--- CaraLibro/LoginViewController.swift
+++ CaraLibro/LoginViewController.swift
@@ -43,41 +43,6 @@ class LoginViewController: UIViewController{
              super.viewDidLoad()
          }
   
-    
-  /*  enum ProviderType: String {
-           case basic
-       }
-       
-       @IBOutlet weak var nombreText: UITextField!
-       @IBOutlet weak var apellidoText: UITextField!
-       @IBOutlet weak var botonLogueo: UIButton!
- 
-   
-   */
-         
-      //   @IBAction func logearActionButon(_ sender: Any) {
-        //     if let apellido = apellidoText.text, let nombre = nombreText.text{
-          //       Auth.auth().createUser(withEmail: apellido, password: nombre){
-            //         (result, error) in
-              //       if let result = result, error == nil{
-                //         print("todo bien")
-                  //   } else {
-                      //   let alertController = UIAlertController(title:"Error", message: "Se ha producido un error", preferredStyle: .alert); alertController.addAction(UIAlertAction(title: "Aceptar", style: .default))
-                       //  self.present(alertController, animated: true, completion: nil)
-                //     }
-                // }
-            // }
-             
-        // }
-       /*  enum AuthenticationSheetView: String, Identifiable{
-             case register
-             case login
-             
-             
-             var id: String{
-                 return rawValue
-             }
-*/
+  
 }
-//}
 
--- CaraLibro/RecoverPasswordViewController.swift
+++ CaraLibro/RecoverPasswordViewController.swift
@@ -6,10 +6,35 @@
 //
 import UIKit
 import Foundation
+import Firebase
 
 class RecoverPasswordViewController:UIViewController {
 
     @IBAction private func tap_Action(sender:UITapGestureRecognizer) {
             self.view.endEditing(true)
+    }
+    
+    
+    @IBOutlet weak var correoTextField: UITextField!
+    @IBAction func CorreoText(_ sender: Any) {
+    }
+    
+    @IBOutlet weak var EnviarBoton: UIButton!
+    
+    
+    @IBAction func RecuperarContrasena(_ sender: Any) {
+        let auth = Auth.auth()
+        
+        auth.sendPasswordReset(withEmail: correoTextField.text!){
+            (error) in if let error = error{
+                let alertController = UIAlertController(title: "Error", message: "Se ha producido un error registrando al usuario", preferredStyle: .alert)
+                alertController.addAction(UIAlertAction(title: "Aceptar", style: .default))
+                self.present(alertController, animated: true, completion: nil)
+                return
+            }
+            let alertController = UIAlertController(title: "Éxito", message: "Se le ha enviado un correo de recuperación", preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: "Aceptar", style: .default))
+            self.present(alertController, animated: true, completion: nil)
+        }
     }
 }
